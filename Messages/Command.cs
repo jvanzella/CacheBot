51a3ddd4df85ddd2d2f285975f934969b3fe7c27
Ryan Musgrave <rmusgrave@pnimedia.com>
2018-02-21 15:03:11 -0800

    Known Type

--- Messages/Command.cs
+++ Messages/Command.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.Serialization;
 
 namespace Messages
 {
+    [KnownType(typeof(RedisCommand))]
     public abstract class Command
     {
         public Environment Environment { get; set; } 
